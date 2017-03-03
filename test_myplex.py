124f8963e7c400adfea288d3ebda4a5ddd1f100d
Dan Anstis <dan@bsod.co.nz>
2017-03-03 19:37:01 +1000

    Update to stop file being overwritten

--- test_myplex.py
+++ test_myplex.py
@@ -30,25 +30,27 @@ def test_singin_with_no_password(mock_isfile):
 def test_generate_token_with_plexshared(mock_urlopen, mock_isfile):
     """ Test token generation using the credentials in user.ini. """
     mock_isfile.return_value = False
-    resp = Mock()
-    resp.read.side_effect = [
-        '<user><authentication-token>Dummy-Auth-Token</authentication-token></user>',
-        '''<Directory type="movie" accessToken="Dummy-Auth-Token" sourceTitle="test"/>
-        <Directory type="movie" accessToken="Dummy-Auth-Token2" sourceTitle="test2"/>''']
-    mock_urlopen.return_value = resp
-    myplex.MYPLEXSHARED = 'disable'
-    assert myplex.myplex_signin('aaa', 'bbb') == 'Dummy-Auth-Token'
+    with patch("__builtin__.open", mock_open(read_data="ExampleToken")):
+        resp = Mock()
+        resp.read.side_effect = [
+            '<user><authentication-token>Dummy-Auth-Token</authentication-token></user>',
+            '''<Directory type="movie" accessToken="Dummy-Auth-Token" sourceTitle="test"/>
+            <Directory type="movie" accessToken="Dummy-Auth-Token2" sourceTitle="test2"/>''']
+        mock_urlopen.return_value = resp
+        myplex.MYPLEXSHARED = 'disable'
+        assert myplex.myplex_signin('aaa', 'bbb') == 'Dummy-Auth-Token'
 
 @patch('myplex.os.path.isfile')
 @patch('myplex.urlopen')
 def test_generate_token_without_plexshared(mock_urlopen, mock_isfile):
     """ Test token generation using the credentials in user.ini. """
     mock_isfile.return_value = False
-    resp = Mock()
-    resp.read.side_effect = [
-        '<user><authentication-token>Dummy-Auth-Token</authentication-token></user>',
-        '''<Directory type="movie" accessToken="Dummy-Auth-Token" sourceTitle="test"/>
-        <Directory type="movie" accessToken="Dummy-Auth-Token2" sourceTitle="test2"/>''']
-    mock_urlopen.return_value = resp
-    myplex.MYPLEXSHARED = 'enable'
-    assert myplex.myplex_signin('aaa', 'bbb') == 'Dummy-Auth-Token2'
+    with patch("__builtin__.open", mock_open(read_data="ExampleToken")):
+        resp = Mock()
+        resp.read.side_effect = [
+            '<user><authentication-token>Dummy-Auth-Token</authentication-token></user>',
+            '''<Directory type="movie" accessToken="Dummy-Auth-Token" sourceTitle="test"/>
+            <Directory type="movie" accessToken="Dummy-Auth-Token2" sourceTitle="test2"/>''']
+        mock_urlopen.return_value = resp
+        myplex.MYPLEXSHARED = 'enable'
+        assert myplex.myplex_signin('aaa', 'bbb') == 'Dummy-Auth-Token2'
